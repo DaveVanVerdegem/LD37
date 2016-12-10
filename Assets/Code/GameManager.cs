0e150cd88a2850ec1a9eaf50f0b11ed7f274783c
Dave Van Verdegem <dave@pajamallama.be>
2016-12-10 18:07:03 +0100

    Game creates grid.

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	#region Inspector Fields
+	[SerializeField]
+	[Tooltip("Prefab to use for tiles.")]
+	/// <summary>
+	/// Prefab to use for tiles.
+	/// </summary>
+	private GameObject _tilePrefab;
 
+	[SerializeField]
+	[Tooltip("Size for the grid to create.")]
+	/// <summary>
+	/// Size for the grid to create.
+	/// </summary>
+	private Vector2 _gridSize;
+	#endregion
+
+	#region Life Cycle
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		Grid.CreateGrid(_gridSize, _tilePrefab);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
+	#endregion
 }
--- Assets/Code/Grid.cs
+++ Assets/Code/Grid.cs
@@ -8,6 +8,15 @@ public class Grid : MonoBehaviour
 
 	#endregion
 
+	#region Fields
+	/// <summary>
+	/// Parent transform for the grid.
+	/// </summary>
+	private static Transform _gridParent;
+
+	private static Tile[,] _grid;
+	#endregion
+
 	#region Life Cycle
 	// Use this for initialization
 	void Start()
@@ -19,6 +28,39 @@ public class Grid : MonoBehaviour
 	void Update()
 	{
 
+	}
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Creates the grid.
+	/// </summary>
+	/// <param name="dimensions">Dimensions for the grid.</param>
+	/// <param name="tilePrefab">Prefab to use for the tiles.</param>
+	public static void CreateGrid(Vector2 dimensions, GameObject tilePrefab)
+	{
+		if(_gridParent != null)
+		{
+			Debug.LogWarning("Grid already created.");
+			return;
+		}
+
+		_gridParent = new GameObject("Grid").transform;
+		_grid = new Tile[(int)dimensions.x, (int)dimensions.y];
+
+		//Vector2 startPosition = new Vector2(-Mathf.FloorToInt(dimensions.x/2), -Mathf.FloorToInt(dimensions.y / 2));
+
+		for(int i = 0; i < dimensions.y; i++)
+		{
+			for(int j = 0; j < dimensions.x; j++)
+			{
+				// Create tile.
+				GameObject newTile = Instantiate(tilePrefab, new Vector2(i, j), Quaternion.identity, _gridParent);
+				newTile.name = i + "-" + j;
+
+				_grid[i, j] = newTile.GetComponent<Tile>();
+			}
+		}
 	}
 	#endregion
 }
