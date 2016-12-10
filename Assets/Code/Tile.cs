5fe47225b7ea81611a740f9af11cad6521309c54
Michel Mohr <fuse.core@hotmail.com>
2016-12-10 17:52:19 +0100

    tilegrid

--- Assets/Code/Tile.cs
+++ Assets/Code/Tile.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Tile : MonoBehaviour {
+    
+    [SerializeField] GameObject stateRockPrefab;
+    [SerializeField] GameObject stateHardrockPrefab;
+    [SerializeField] GameObject stateDugPrefab;
+    
+    GameObject stateVisual;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum state { Rock, Hardrock, Dug}
+    [SerializeField] state tileState = state.Rock;
+
+    
+    public void Initialize(state spawnState) {
+        switch(tileState) {
+            case state.Rock:
+                stateVisual = Instantiate(stateRockPrefab, transform.position, transform.rotation, transform);
+                break;
+            case state.Hardrock:
+                stateVisual = Instantiate(stateHardrockPrefab, transform.position, transform.rotation, transform);
+                break;
+            case state.Dug:
+                stateVisual = Instantiate(stateDugPrefab,   transform.position, transform.rotation, transform);
+                break;
+        }
+    }
+
+    void OnMouseDown() {
+        if(tileState==state.Rock) {
+            Destroy(stateVisual);
+            stateVisual = Instantiate(stateDugPrefab, transform.position, transform.rotation, transform); 
+        }
+    }
 }
