b788c0ac72db364d6121dcf805918a04ccd35aed
Willowblade <laurent@pozyx.io>
2016-12-11 03:32:14 +0100

    Movement, attacking, etc basics implemented :)

--- Assets/Code/Character.cs
+++ Assets/Code/Character.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// GetDistanceToTarget opkuisen met getdistance etc voor of de state momenteel in combat, alert, of idle is.
+// alert loskoppelen van combat.
+// enum onderzoeken.
+
 public class Character : MonoBehaviour {
 
+    // character core attributes
     public int MaxHealth;
-    public float MovementSpeed;
+    public float MovementSpeedIdle;
+    public float MovementSpeedCombat;
     public float MinBaseDamage;
     public float MaxBaseDamage;
     public float AttackSpeedSeconds;
     public float AttackDistance;
     public float MaxIdleMovement;
     public float MaxIdleWaitSeconds;
-    
+    public float MaxAlertnessDurationSeconds;
+
+    public float IdleDetectionRadius;
+    public float CombatDetectionRadius;
+
     // use this for future additions
+    public string Group;
     public string BattleType;
     public List<string> Skills;
 
@@ -23,6 +34,7 @@ public class Character : MonoBehaviour {
     private float _attackTimer = 0.0f;
     private float _idleMovementTimer = 0.0f;
     private float _idleWaitTimer = 0.0f;
+    private float _alertnessTimer = 0.0f;
     private bool _newIdlePositionChosen = false;
 
     private Vector2 _newIdleMovePosition;
@@ -40,6 +52,7 @@ public class Character : MonoBehaviour {
 	void Start ()
     {
         _currentHealth = MaxHealth;
+        _alertnessTimer = MaxAlertnessDurationSeconds; // causes the monster to not be alert at first
         SwitchToIdleState();
 	}
 	
@@ -60,10 +73,29 @@ public class Character : MonoBehaviour {
 
     }
 
-    void MoveTo(Vector2 destination)
+    void MoveTo(Vector2 destination, float MovementSpeed)
     {
+        // TODO implement pathfinding here proper.
         float MoveStep = MovementSpeed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position,destination, MoveStep);
+    }
+
+    void DetectEnemiesInArea(float detectionRadius)
+    {
+        Collider2D[] Colliders = Physics2D.OverlapCircleAll(transform.position, detectionRadius);
+        foreach (Collider2D Collider in Colliders)
+        {
+            if (Collider.GetComponent<Character>().Group != Group)
+            {
+                if (Physics2D.Raycast(transform.position, transform.position - Collider.transform.position))
+                {
+                    _currentTarget = Collider.GetComponent<Character>();
+                    _currentTarget.AddTarget(GetComponent<Character>());
+                    SwitchToCombatState();
+                    return;
+                }
+            }
+        }
     }
 
     #region CharacterIdle
@@ -74,6 +106,7 @@ public class Character : MonoBehaviour {
         {
             CharacterIdleMove();
         }
+        DetectEnemiesInArea(IdleDetectionRadius);
     }
 
     void CharacterIdleMove()
@@ -90,13 +123,13 @@ public class Character : MonoBehaviour {
         }
         else
         {
-            MoveTo(_newIdleMovePosition);
+            MoveTo(_newIdleMovePosition, MovementSpeedIdle);
         }
-
     }
 
     void SwitchToIdleState()
     {
+        _inCombat = false;
         _idleMovementTimer = 0.0f;
     }
 
@@ -124,38 +157,83 @@ public class Character : MonoBehaviour {
         {
             SelectNewTarget();
         }
-        CharacterCombatMove();
+        else
+        {
+            CharacterCombatMove();
+            if (_attackTimer > AttackSpeedSeconds)
+            {
+                if (GetDistanceToTarget() < AttackDistance)
+                {
+                    CharacterAttack();
+                }
+            }
+        }
+    }
+
+ 
+    float GetDistanceToTarget()
+    {
+        if (_inCombat)
+        {
+            if (_currentTarget == null)
+            {
+                return Vector2.Distance(transform.position, _newIdleMovePosition);
+            }
+            else
+            {
+                return Vector2.Distance(transform.position, _currentTarget.transform.position);
+            }
+        }
+        else
+        {
+            return Vector2.Distance(transform.position, _newIdleMovePosition);
+        }
     }
 
     void CharacterCombatMove()
     {
-
-    }
-
-    List<GameObject> GetTargetsInView()
-    {
-        List<GameObject> Targets = new List<GameObject>();
-        return Targets;
+        SetAnimation("CombatMove");
+        if (GetDistanceToTarget() > AttackDistance)
+        {
+            MoveTo(_currentTarget.transform.position, MovementSpeedCombat);
+        }
     }
 
     void SelectNewTarget()
     {
-        // if no target in range ->
-        List<GameObject> TargetsInView = GetTargetsInView();
-        if (TargetsInView.Count == 0)
+        // if no target in range -> search for new target with high radius in alert duration
+        DetectEnemiesInArea(CombatDetectionRadius);
+        if (_currentTarget == null)
+        {
+            // this should provide smooth overlap to a slower walk towards the same idle destination
+            // once the alert phase is over.
+            _alertnessTimer += Time.deltaTime;
+            SetAnimation("CombatMove");
+            if (!_newIdlePositionChosen)
+            {
+                StartIdleWait();
+                SetNewIdlePosition();
+            }
+            if (Vector2.Distance(_newIdleMovePosition, transform.position) == 0f)
+            {
+                SetNewIdlePosition();
+            }
+            else
+            {
+                MoveTo(_newIdleMovePosition, MovementSpeedCombat);
+            }
+        }
+        if (_alertnessTimer > MaxAlertnessDurationSeconds)
         {
             SwitchToIdleState();
-        }
-        else
-        {
-            
         }
         
     }
         
     void CharacterAttack()
     {
-        // Time.deltaTime;
+        _attackTimer = 0.0f;
+        SetAnimation("Attack");
         int damageDealt = (int)(Random.Range(MaxBaseDamage, MaxBaseDamage) * _damageDealtModifier);
         _currentTarget.ReceiveDamage(damageDealt);
     }
@@ -171,7 +249,13 @@ public class Character : MonoBehaviour {
 
     void SwitchToCombatState()
     {
+        _inCombat = true;
+        _attackTimer = 0.0f;
+    }
 
+    void AddTarget(Character character)
+    {
+        _targetedBy.Add(character);
     }
     #endregion
 
@@ -184,11 +268,13 @@ public class Character : MonoBehaviour {
         }
         // do death animation
         _isAlive = false;
+        Destroy(gameObject);
     }
 
     void RemoveTarget()
     {
         _currentTarget = null;
+        _alertnessTimer = 0.0f;
     }
     #endregion
 }
