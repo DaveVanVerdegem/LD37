8062909f5d798473ef2bf6a72044ec9dd21ba252
Reskaillev <unknown-50@hotmail.com>
2016-12-12 04:00:40 +0100

    Fixed level preset update process

--- Assets/Code/TileGrid.cs
+++ Assets/Code/TileGrid.cs
@@ -122,7 +122,7 @@ public class TileGrid : MonoBehaviour {
             for(int y = 0; y < gridHeight; y++)
 			{
 				_tileList[x, y].setNeighbouringTiles(getNeighbouringTiles(x,y));
-				_tileList[x,y].updateTileGraphics(new int[2]);
+				_tileList[x,y].updateTileGraphics();
 			}
         }
     }
